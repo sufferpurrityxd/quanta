130483fa1a7fa7616df2d4dbac23432537d4584c
suferpurrityxd <11111lesha714@gmail.com>
2023-06-29 00:55:19 +1100

    new arch

--- quanta-artifact/src/lib.rs
+++ quanta-artifact/src/lib.rs
@@ -11,10 +11,5 @@ mod ziplib;
 /// network are called artifacts.
 ///
 /// An artifact is a file that is
-/// distributed directly from the user's computer. This means that we do not save
-/// the file in any way and do not convert it locally. All hash calculations, magnet
-/// links, etc. happen at runtime. This approach has pros and cons.
-///
-/// Cons - the file may disappear due to the user's inattention
-/// Pros - we do not create a new instance and do not clog memory once again
+/// distributed directly from the user's computer.
 pub(crate) const MAX_ARTIFACT_SIZE: usize = 2048;
--- quanta-walker/src/lib.rs
+++
@@ -1,4 +0,0 @@
-/// quanta-walker - This is a utility that 'runs' through all the files in
-/// the system and checks for the presence of files that are distributed by the peer.
-/// If they were deleted / moved somewhere, then quanta updates the repository
-type _TODO = usize;
